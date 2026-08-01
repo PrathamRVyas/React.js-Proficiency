94057b88052f04f1a8dc04f071e1c4c1b2423989
Pratham R Vyas <prathamrvyas@gmail.com>
2026-08-01 23:44:26 +0530

    Why we need hooks - using useState

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0)
+
+  const addValue = () => {
+      // console.log("clicked", Math.random())
+      // counter = counter + 1
+      // console.log(counter)
+      if(counter < 20){
+        setCounter(counter + 1)
+      }
+  }
+
+  const removeValue = () =>{
+    if(counter > 0){
+    setCounter(counter - 1)
+    }
+  }
+
+  return (
+    <>
+    <h1>Chai aur react</h1>
+    <h2>Counter value: {counter}</h2>
+
+    <button
+    onClick={addValue}
+    >Add value {counter}</button>
+    <br/>
+    <button
+    onClick={removeValue}>Remove value {counter}</button>
+    <footer>{counter}</footer>
+    </>  
+  )
+}
+
+export default App
