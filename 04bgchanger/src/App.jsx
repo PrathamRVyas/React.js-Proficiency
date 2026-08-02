4ceccc876e8fecda806019ab14f665abfbb4ae0a
Pratham R Vyas <prathamrvyas@gmail.com>
2026-08-03 01:48:12 +0530

    background color changer app

---
+++ 04bgchanger/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+function App() {
+  const [color, setColor] = useState("olive")
+
+  const changeColor = (e) =>{
+    setColor(e.target.style.backgroundColor)
+  }
+
+  return (
+    <>
+      <div className="w-full h-screen duration-200" style={{backgroundColor: color}}></div>
+
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button onClick={changeColor} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "red"}}>Red</button>
+          <button onClick={changeColor} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "green"}}>Green</button>
+          <button onClick={changeColor} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "blue"}}>Blue</button>
+          <button onClick={changeColor} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "pink"}}>Pink</button>
+          <button onClick={changeColor} className="outline-none px-4 py-1 rounded-full text-black shadow-lg" style={{backgroundColor: "yellow"}}>Yellow</button>
+          <button onClick={changeColor} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "orange"}}>Orange</button>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
